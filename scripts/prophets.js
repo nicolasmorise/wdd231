737c61cec1cd6e8999a7d137d9b71fa5b31df8cf
Nicolas Eiji <nicolas.eiji.morise@gmail.com>
2025-01-14 11:41:14 -0300

    commit

---
+++ scripts/prophets.js
@@ -0,0 +1,32 @@
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2'); // Replace '__' with 'h2'
+        let portrait = document.createElement('img');
+
+        // Replace '____' with appropriate property names from the JSON data
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; 
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName); // Replace '_______' with 'fullName'
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+};
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+}
+
+getProphetData();
