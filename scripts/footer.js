dcb06b48ec6c032d86f7a05196cf0e5af9c0b4e7
Nicolas Eiji <nicolas.eiji.morise@gmail.com>
2025-07-06 19:14:51 -0300

    commit

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,6 @@
-let x = document.lastModified;
+const lastModified = document.lastModified;
+const currentYear = new Date().getFullYear();
+const copyrightSymbol = '\u00A9';
 
-document.getElementById('last-modified').innerHTML = `Last Modified: ${x}`
-
-console.log(x)
+document.getElementById('last-modified').textContent = `Last Modified: ${lastModified}`;
+document.getElementById('copyright').textContent = `${copyrightSymbol} ${currentYear}`;
